50b5c3da863642a582767d39d98a2871a3d99162
Kajal2722 <25b2448@iitb.ac.in>
2026-06-23 23:08:22 +0530

    Create MessageInput.jsx

---
+++ Week-3/MessageInput.jsx
@@ -0,0 +1,16 @@
+mport React from 'react'
+
+const MessageInput = () => {
+  return (
+    <>
+    <div>
+       <div class="msgarea">
+            <input type="text" placeholder="Type a message.." / >
+            <button>Send</button>
+        </div>
+    </div>
+    </>
+  );
+}
+
+export default MessageInput;
